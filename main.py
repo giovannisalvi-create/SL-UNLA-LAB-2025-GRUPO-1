54c41ccab13d92c0b6db7f62b05641ddb11945f6
giovanni <giovannisalvi05@gmail.com>
2025-09-21 15:54:21 -0300

    Levantamiento de api

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print ("Hello world")
-print ("Goodbye")
-print ("Chau")
+from fastapi import FastAPI
+from database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+app = FastAPI(title="TP - API de Turnos")
+
+@app.get("/", include_in_schema=False)
+def root():
+    return {"mensaje": "API de turnos funcionando. Ir a /docs para la documentación."}
