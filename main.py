235dc0d9cfd0e329fd1272457a5c7297d6d2ac00
giovanni <giovannisalvi05@gmail.com>
2025-09-22 00:43:09 -0300

    agregado de endpoint finales

--- main.py
+++ main.py
@@ -166,3 +166,25 @@ def obtener_turno(turno_id: int, db: Session = Depends(get_db)):
         persona_id=t.persona_id,
         dni=persona.dni
     )
+
+@app.put("/turnos/{turno_id}", response_model=schemas.TurnoOut)
+def actualizar_turno(turno_id: int, turno_up: schemas.TurnoUpdate, db: Session = Depends(get_db)):
+    t = crud.update_turno(db, turno_id, turno_up)
+    if not t:
+        raise HTTPException(status_code=404, detail="Turno no encontrado")
+    t_con_persona = db.query(models.Turno).join(models.Persona).filter(models.Turno.id == turno_id).first()
+    return schemas.TurnoOut(
+        id=t_con_persona.id,
+        fecha=t_con_persona.fecha,
+        hora=t_con_persona.hora,
+        estado=t_con_persona.estado,
+        persona_id=t_con_persona.persona_id,
+        dni=t_con_persona.persona.dni
+    )
+
+@app.delete("/turnos/{turno_id}")
+def eliminar_turno(turno_id: int, db: Session = Depends(get_db)):
+    ok = crud.delete_turno(db, turno_id)
+    if not ok:
+        raise HTTPException(status_code=404, detail="Turno no encontrado")
+    return {"ok": True, "mensaje": "Turno eliminado"}
