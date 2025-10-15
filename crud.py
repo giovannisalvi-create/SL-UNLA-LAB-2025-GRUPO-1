a91b81c1adce0e01f8a51cc017e58ccc5e2b52a7
Brian Rodriguez <brianrodriguezpsn@gmail.com>
2025-10-15 00:01:18 -0300

    Se cambió el nombre de algunas variables para mejorar su legibilidad.
    -Se renombraron variables por nombres más descriptivos (persona_db, turno_db, persona_creada, etc)
    -Implementación de services.calcular_edad() para eliminar código duplicado
    -Actualizar a Pydantic v2. Cambio de .dict() a .model_dump()

--- crud.py
+++ crud.py
@@ -3,11 +3,11 @@ from typing import List, Optional
 import models, schemas
 
 def create_persona(db: Session, persona_in: schemas.PersonaCreate) -> models.Persona:
-    p = models.Persona(**persona_in.dict())
-    db.add(p)
+    persona_db = models.Persona(**persona_in.model_dump())
+    db.add(persona_db)
     db.commit()
-    db.refresh(p)
-    return p
+    db.refresh(persona_db)
+    return persona_db
 
 def get_personas(db: Session, skip: int = 0, limit: int = 100) -> List[models.Persona]:
     return db.query(models.Persona).offset(skip).limit(limit).all()
@@ -17,30 +17,30 @@ def get_persona(db: Session, persona_id: int) -> Optional[models.Persona]:
 
 
 def update_persona(db: Session, persona_id: int, persona_up: schemas.PersonaUpdate) -> Optional[models.Persona]:
-    p = get_persona(db, persona_id)
-    if not p:
+    persona_db = get_persona(db, persona_id)
+    if not persona_db:
         return None
-    for k, v in persona_up.dict(exclude_unset=True).items():
-        setattr(p, k, v)
-    db.add(p)
+    for campo, valor in persona_up.model_dump(exclude_unset=True).items():
+        setattr(persona_db, campo, valor)
+    db.add(persona_db)
     db.commit()
-    db.refresh(p)
-    return p
+    db.refresh(persona_db)
+    return persona_db
 
 def delete_persona(db: Session, persona_id: int) -> bool:
-    p = get_persona(db, persona_id)
-    if not p:
+    persona_a_eliminar = get_persona(db, persona_id)
+    if not persona_a_eliminar:
         return False
-    db.delete(p)
+    db.delete(persona_a_eliminar)
     db.commit()
     return True
 
 def create_turno(db: Session, turno_in: schemas.TurnoCreate) -> models.Turno:
-    t = models.Turno(**turno_in.dict())
-    db.add(t)
+    turno_db = models.Turno(**turno_in.model_dump())
+    db.add(turno_db)
     db.commit()
-    db.refresh(t)
-    return t
+    db.refresh(turno_db)
+    return turno_db
 
 def get_turnos(db: Session, skip: int = 0, limit: int = 100) -> List[models.Turno]:
     return db.query(models.Turno).offset(skip).limit(limit).all()
@@ -52,20 +52,20 @@ def get_persona_por_dni(db: Session, dni: str) -> Optional[models.Persona]:
     return db.query(models.Persona).filter(models.Persona.dni == dni).first()
 
 def update_turno(db: Session, turno_id: int, turno_up: schemas.TurnoUpdate) -> Optional[models.Turno]:
-    t = get_turno(db, turno_id)
-    if not t:
+    turno_db = get_turno(db, turno_id)
+    if not turno_db:
         return None
-    for k, v in turno_up.dict(exclude_unset=True).items():
-        setattr(t, k, v)
-    db.add(t)
+    for campo, valor in turno_up.model_dump(exclude_unset=True).items():
+        setattr(turno_db, campo, valor)
+    db.add(turno_db)
     db.commit()
-    db.refresh(t)
-    return t
+    db.refresh(turno_db)
+    return turno_db
 
 def delete_turno(db: Session, turno_id: int) -> bool:
-    t = get_turno(db, turno_id)
-    if not t:
+    turno_a_eliminar = get_turno(db, turno_id)
+    if not turno_a_eliminar:
         return False
-    db.delete(t)
+    db.delete(turno_a_eliminar)
     db.commit()
     return True
